6b2370d575f5ecb66d7e696531bc716631228aa2
Ankit <ankitdj13112002@gmail.com>
2025-06-27 20:44:33 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.css";
+import Gallery from "./components/gallery/Gallery";
+import LeftBar from "./components/leftBar/LeftBar";
+import Topbar from "./components/topBar/Topbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <LeftBar />
+      <div className="content">
+        <Topbar />
+        <Gallery />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/leftBar/LeftBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./leftBar.css";
+
+const LeftBar = () => {
+  return (
+    <div>
+      <div className="menuIcons"></div>
+    </div>
+  );
+};
+
+export default LeftBar;
