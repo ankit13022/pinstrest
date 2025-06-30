474e1be1a68f3d1cb5486ca0b71e8db95876e574
Ankit <ankitdj13112002@gmail.com>
2025-07-01 00:51:06 +0530

    integretion few stem

--- client/src/components/userButton/UserButton.jsx
+++ client/src/components/userButton/UserButton.jsx
@@ -1,38 +1,48 @@
 import { useState } from "react";
 import "./userButton.css";
-import Image from "../image/Image";
-
+import Image from "../../components/image/image";
+import apiRequest from "../../utils/apiRequest";
+import { Link, useNavigate } from "react-router";
+import useAuthStore from "../../utils/authStore";
 const UserButton = () => {
   const [open, setOpen] = useState(false);
-  const currentuser = true;
 
-  return currentuser ? (
+  const navigate = useNavigate();
+
+  const { currentUser, removeCurrentUser } = useAuthStore();
+
+  const handleLogout = async () => {
+    try {
+      await apiRequest.post("/users/auth/logout", {});
+      removeCurrentUser();
+      navigate("/auth");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return currentUser ? (
     <div className="userButton">
-      <Image path="general/noAvatar.png" alt="" />
-      <Image
-        onClick={() => setOpen((prev) => !prev)}
-        path="general/arrow.svg"
-        alt=""
-        className="arrow"
-      />
+      <Image path={currentUser.img || "/general/noAvatar.png"} alt="" />
+      <div onClick={() => setOpen((prev) => !prev)}>
+        <Image path="/general/arrow.svg" alt="" className="arrow" />
+      </div>
       {open && (
         <div className="userOptions">
-          <div className="userOption">
-            <span>Profile</span>
-          </div>
-          <div className="userOption">
-            <span>Settings</span>
-          </div>
-          <div className="userOption">
-            <span>Logout</span>
+          <Link to={`/profile/${currentUser.username}`} className="userOption">
+            Profile
+          </Link>
+          <div className="userOption">Setting</div>
+          <div className="userOption" onClick={handleLogout}>
+            Logout
           </div>
         </div>
       )}
     </div>
   ) : (
-    <a href="/" className="loginlink">
-      login/signup
-    </a>
+    <Link to="/auth" className="loginLink">
+      Login / Sign Up
+    </Link>
   );
 };
 
