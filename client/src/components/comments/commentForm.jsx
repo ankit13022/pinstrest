33193aac734078fab3383f283e7bb24948b2f4fe
Ankit <ankitdj13112002@gmail.com>
2025-07-01 19:40:41 +0530

    comment added

--- client/src/components/comments/commentForm.jsx
+++ client/src/components/comments/commentForm.jsx
@@ -1,16 +1,55 @@
-import React, { useState } from "react";
-import "./comments.css";
 import EmojiPicker from "emoji-picker-react";
-const commentForm = () => {
+import { useState } from "react";
+import apiRequest from "../../utils/apiRequest";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const addComment = async (comment) => {
+  const res = await apiRequest.post("/comments", comment);
+  return res.data;
+};
+
+const CommentForm = ({ id }) => {
   const [open, setOpen] = useState(false);
+  const [desc, setDesc] = useState("");
+
+  const handleEmojiClick = (emoji) => {
+    setDesc((prev) => prev + " " + emoji.emoji);
+    setOpen(false);
+  };
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: addComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", id] });
+      setDesc("");
+      setOpen(false);
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    mutation.mutate({
+      description: desc,
+      pin: id,
+    });
+  };
+
   return (
-    <form className="commentForm">
-      <input type="text" placeholder="Add a comment" />
+    <form className="commentForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a comment"
+        onChange={(e) => setDesc(e.target.value)}
+        value={desc}
+      />
       <div className="emoji">
         <div onClick={() => setOpen((prev) => !prev)}>😊</div>
         {open && (
           <div className="emojiPicker">
-            <EmojiPicker />
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
           </div>
         )}
       </div>
@@ -18,4 +57,4 @@ const commentForm = () => {
   );
 };
 
-export default commentForm;
+export default CommentForm;
